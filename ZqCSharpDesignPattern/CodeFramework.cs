b3118e7ed61685b5051142fd9c18bf8b7b20f4fe
ZORO-1104 <zhangqi_1104@163.com>
2020-07-28 09:26:33 +0800

    项目基本框架

---
+++ ZqCSharpDesignPattern/CodeFramework.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZqCSharpDesignPattern
+{
+    abstract class CodeFramework
+    {
+        protected abstract void TestDescription();
+        protected abstract void RunCode();
+
+        public void Execute()
+        {
+            Console.WriteLine($"<<<<<<测试开始>>>>>>");
+            TestDescription();
+            RunCode();
+
+            Console.WriteLine($"<<<<<<测试结束>>>>>>");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("【提示】：按任意键退出。。。");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ZqCSharpDesignPattern/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ZqCSharpDesignPattern.BehavioralPattern.C0101_ObserverPattern;
+using ZqCSharpDesignPattern.BehavioralPattern.C0301_StrategyPattern;
+using ZqCSharpDesignPattern.CreationalPattern.A0101_SimpleFactory;
+using ZqCSharpDesignPattern.CreationalPattern.A0201_FactoryMethod;
+using ZqCSharpDesignPattern.CreationalPattern.A0301_AbstractFactory;
+using ZqCSharpDesignPattern.CreationalPattern.A0401_SingletonPattern;
+using ZqCSharpDesignPattern.CreationalPattern.A0601_BuilderPattern;
+
+namespace ZqCSharpDesignPattern
+{
+    class Program
+    {
+        static CodeFramework cf = null;
+
+        static void Main(string[] args)
+        {
+            RunMethod();
+
+            cf.Execute();
+        }
+
+        private static void RunMethod()
+        {
+            cf = new A0101SimpleFactory();
+            cf = new A0201FactoryMethod();
+            cf = new A0301AbstractFactory();
+            cf = new A0601BuilderPattern();
+            cf = new A0401SingletonPattern();
+            cf = new C0301StrategyPattern();
+            cf = new C0101ObserverPattern();
+        }
+    }
+}
