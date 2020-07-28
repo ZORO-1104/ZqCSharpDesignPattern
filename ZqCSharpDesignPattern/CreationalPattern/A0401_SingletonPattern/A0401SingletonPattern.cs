177ac581df33dc3b7f5a54210a964c8bbfdfb809
ZORO-1104 <zhangqi_1104@163.com>
2020-07-28 09:33:06 +0800

    单例模式

---
+++ ZqCSharpDesignPattern/CreationalPattern/A0401_SingletonPattern/A0401SingletonPattern.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZqCSharpDesignPattern.CreationalPattern.A0401_SingletonPattern
+{
+    class A0401SingletonPattern : CodeFramework
+    {
+        protected override void RunCode()
+        {
+            Test01();
+        }
+
+        private void Test01()
+        {
+            President p1 = President.GetPresident();
+            Console.WriteLine($"count={President.Count}");
+
+            President p2 = President.GetPresident();
+            Console.WriteLine($"count={President.Count}");
+
+            President p3 = President.GetPresident();
+            President p4 = President.GetPresident();
+            Console.WriteLine($"count={President.Count}");
+        }
+
+        protected override void TestDescription()
+        {
+            Console.WriteLine("【单例模式 - 01】");
+        }
+    }
+}
---
+++ ZqCSharpDesignPattern/CreationalPattern/A0401_SingletonPattern/President.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZqCSharpDesignPattern.CreationalPattern.A0401_SingletonPattern
+{
+    class President
+    {
+        private static President president;
+        private static readonly object sync = new object();
+
+        private static int count = 0;
+        public static int Count { get => count;}
+
+        private President()
+        {
+            count++;
+        }
+
+        public static President GetPresident()
+        {
+            if (null==president)
+            {
+                lock (sync)
+                {
+                    if (null==president)
+                    {
+                        president = new President();
+                    }
+                }
+            }
+
+            return president;
+        }
+    }
+}
