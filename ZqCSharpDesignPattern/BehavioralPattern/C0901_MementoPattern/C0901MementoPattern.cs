37d0491a6f83a9268e2e6b10dc4286bbc0ec10f4
ZORO-1104 <zhangqi_1104@163.com>
2020-08-05 14:23:21 +0800

    备忘录模式 - 仅保存一个备忘信息

---
+++ ZqCSharpDesignPattern/BehavioralPattern/C0901_MementoPattern/C0901MementoPattern.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZqCSharpDesignPattern.BehavioralPattern.C0901_MementoPattern
+{
+    class C0901MementoPattern : CodeFramework
+    {
+        protected override void RunCode()
+        {
+            Test01();
+        }
+
+        private void Test01()
+        {
+            Originator o1 = new Originator();
+            Caretaker c1 = new Caretaker();
+
+            o1.Name = "张三";
+            o1.Age = 20;
+            o1.ShowInfo();
+
+            //保存信息
+            c1.SetMemento(o1.CreateMemento());
+
+            o1.Name = "李四";
+            o1.Age = 25;
+            o1.ShowInfo();
+
+            //恢复信息
+            o1.SetMemento(c1.GetMemento());
+            o1.ShowInfo();
+        }
+
+        protected override void TestDescription()
+        {
+            Console.WriteLine("【备忘录模式 - 01】");
+        }
+    }
+}
---
+++ ZqCSharpDesignPattern/BehavioralPattern/C0901_MementoPattern/Caretaker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZqCSharpDesignPattern.BehavioralPattern.C0901_MementoPattern
+{
+    class Caretaker
+    {
+        private Memento memento;
+
+        public void SetMemento(Memento m)
+        {
+            memento = m;
+        }
+
+        public Memento GetMemento()
+        {
+            return memento;
+        }
+    }
+}
