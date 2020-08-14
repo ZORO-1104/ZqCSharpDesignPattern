a7b34ee7326825131e0213265cd36aa72be79260
ZORO-1104 <zhangqi_1104@163.com>
2020-08-14 10:51:18 +0800

    建造者模式，修改客户端调用

--- ZqCSharpDesignPattern/CreationalPattern/A0601_BuilderPattern/A0601BuilderPattern.cs
+++ ZqCSharpDesignPattern/CreationalPattern/A0601_BuilderPattern/A0601BuilderPattern.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ZqCSharpDesignPattern.CreationalPattern.A0601_BuilderPattern
 {
-    class A0601BuilderPattern : CodeFramework
+    internal class A0601BuilderPattern : CodeFramework
     {
         protected override void RunCode()
         {
@@ -13,17 +13,24 @@ namespace ZqCSharpDesignPattern.CreationalPattern.A0601_BuilderPattern
 
         private void Test01()
         {
-            Product p = null;
-            Director d = null;
+            //客户端使用建造者模式中的角色
+            Product p = null;//产品
+            Director d = null;//指挥者
+            AbstractBuilder absBud = null;//抽象建造者
 
-            //d = new Director(new ConcreteBuilderA());
-            d = new Director(new ConcreteBuilderB());
+            //实例化抽象建造者，将其实例化为具体的建造者
+            //absBud = new ConcreteBuilderA();
+            absBud = new ConcreteBuilderB();
 
-            //具体建造过程，对客户端来说是隐藏的（避免了客户端使用方由于对产品认识不全面，造成建造）。
+            //实例化指挥者（将抽象建造者通过构造器注入进去）
+            d = new Director(absBud);
+
+            //具体建造过程，对客户端来说是隐藏的（避免了客户端使用方由于对产品认识不全面，造成建造错误）。
             //其产品建造的完整性，由Director来保证。
             //客户端只管调用Construct方法，来建造产品即可。
             p = d.Construct();
 
+            //调用产品的通用接口ShowProduct
             p.ShowProduct();
         }
 
