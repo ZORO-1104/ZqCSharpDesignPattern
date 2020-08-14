094cf450c3ce1a86f89519626c492a94e8926e3b
ZORO-1104 <zhangqi_1104@163.com>
2020-08-14 13:35:17 +0800

    单例模式，修改调用客户端

--- ZqCSharpDesignPattern/CreationalPattern/A0401_SingletonPattern/A0401SingletonPattern.cs
+++ ZqCSharpDesignPattern/CreationalPattern/A0401_SingletonPattern/A0401SingletonPattern.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace ZqCSharpDesignPattern.CreationalPattern.A0401_SingletonPattern
 {
-    class A0401SingletonPattern : CodeFramework
+    internal class A0401SingletonPattern : CodeFramework
     {
         protected override void RunCode()
         {
@@ -22,6 +22,29 @@ namespace ZqCSharpDesignPattern.CreationalPattern.A0401_SingletonPattern
             President p3 = President.GetPresident();
             President p4 = President.GetPresident();
             Console.WriteLine($"count={President.Count}");
+
+            Console.WriteLine("判断是否相等");
+            Console.WriteLine(p1 == p2);
+            Console.WriteLine(p1 == p3);
+            Console.WriteLine(p1 == p4);
+            Console.WriteLine(p1.GetHashCode().ToString());
+            Console.WriteLine(p2.GetHashCode().ToString());
+            Console.WriteLine(p3.GetHashCode().ToString());
+            Console.WriteLine(p4.GetHashCode().ToString());
+
+            /*
+            count=1
+            count=1
+            count=1
+            判断是否相等
+            True
+            True
+            True
+            58225482
+            58225482
+            58225482
+            58225482
+             */
         }
 
         protected override void TestDescription()
